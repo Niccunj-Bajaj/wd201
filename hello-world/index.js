dc56dd52473b051f88e43f2fc4ef56db6599e083
Niccunj Bajaj <niccunjbajaj2006@gmail.com>
2024-09-04 16:06:53 +0530

    Initial

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,5 +1,22 @@
-const hello = () => {
-  console.log("Hello GitHub!");
-};
-
-hello();
+// fs.writeFile(
+//   "sample.txt",
+//   "Hello World. Welcome to Node.js File System module.",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("File created!");
+//   }
+// );
+// fs.readFile("sample.txt", (err, data) => {
+//   if (err) throw err;
+//   console.log(data.toString());
+// });
+const fs = require("fs");
+const http = require("http");
+const server = http.createServer((req, res) => {
+  const stream = fs.createReadStream("sample.txt");
+  stream.pipe();
+  // fs.readFile("sample.txt", (err, data) => {
+  //   res.end(data);
+  // });
+});
+server.listen(3000);
---
+++ index.js
@@ -0,0 +1,6 @@
+let hello = "Hello";
+const returnHello = () => {
+  console.log(hello);
+};
+
+returnHello();
