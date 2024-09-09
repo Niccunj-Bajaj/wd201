52e2e13d5462e966c36b672c1e172034faf924fc
Niccunj Bajaj <niccunjbajaj2006@gmail.com>
2024-09-09 16:15:27 +0530

    Update

---
+++ todo-cli/__tests__/todo.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-undef */
+var dateToday = new Date();
+const formattedDate = (d) => {
+  return d.toISOString().split("T")[0];
+};
+const today = formattedDate(dateToday);
+const yesterday = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() - 1)),
+);
+const todoList = require("../todo");
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
+describe("Todolist test suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toISOString(),
+    });
+    add({ title: "Submit assignment", dueDate: yesterday, completed: false });
+    add({ title: "Service Vehicle", dueDate: today, completed: false });
+    add({ title: "Pay rent", dueDate: today, completed: true });
+  });
+  test("Should add new todo", () => {
+    const todoItems = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toISOString(),
+    });
+    expect(all.length).toBe(todoItems + 1);
+  });
+  test("Should mark completed", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+  test("Should retrive overdue", () => {
+    let over = overdue();
+    expect(over.length).toBe(over.length);
+  });
+  test("Should retrive dueToday", () => {
+    let today = dueToday();
+    expect(today.length).toBe(today.length);
+  });
+  test("Should retrive dueLater", () => {
+    let later = dueLater();
+    expect(later.length).toBe(later.length);
+  });
+});
