f1b3e204eda52ae71bee94b29b4c6010b838fe36
Niccunj Bajaj <niccunjbajaj2006@gmail.com>
2024-09-12 02:46:31 +0530

    Add pre-commit hook to run tests

--- todo-cli/eslint.config.mjs
+++ todo-cli/eslint.config.mjs
@@ -1,8 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
+
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
 ];
